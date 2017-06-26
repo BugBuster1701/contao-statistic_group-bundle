38ae17f3c560df62c3c081e9f28ddf850bdf940b
Glen Langer <contao.modul@glen-langer.de>
2017-06-27 01:38:01 +0200

    Add ru and pl language files

---
+++ src/Resources/contao/languages/pl/modules.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://docs.transifex.com/faq/#translating
+ * @link https://www.transifex.com/projects/p/contao-statistic_group-bundle/language/pl/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2017-06-27T01:37:32+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['statistics'] = 'Statystyka';
+
